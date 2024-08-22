99732ac9930ec35bf2e8b82fa4bd6496dceb2bb8
roycekings <zeparas34@gmail.com>
2024-08-22 10:48:58 +0100

    creation des routes des differentes pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-
+import {Route,Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/Signin'
+import Profile from './pages/Profile'
+import SignUp from './pages/SignUp'
+import About from './pages/about'
 
 function App() {
  
 
   return (
-    <>
-      
-     
-    </>
+    <Routes>
+      <Route path='/' element = {<Home />} />
+      <Route path='/about' element = {<About />} />
+      <Route path='/signin' element = {<SignIn />} />
+      <Route path='/signout' element = {<SignUp />} />
+      <Route path='/profile' element = {<Profile />} />
+    </Routes>
   )
 }
 
---
+++ src/pages/Signin.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <div>Signin</div>
+  )
+}
+
+export default SignIn
